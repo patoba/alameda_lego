569237c95b7eec7fb7c8316f71fc26d5c71c5110
Pato <30847666+patoba@users.noreply.github.com>
2020-05-11 01:52:26 -0500

    Update Window.cpp

--- ProyectoFinal/Window.cpp
+++ ProyectoFinal/Window.cpp
@@ -25,10 +25,10 @@ Window::Window(GLint windowWidth, GLint windowHeight)
 }
 int Window::Initialise()
 {
-	//Inicialización de GLFW
+	//InicializaciÃ³n de GLFW
 	if (!glfwInit())
 	{
-		printf("Falló inicializar GLFW");
+		printf("FallÃ³ inicializar GLFW");
 		glfwTerminate();
 		return 1;
 	}
@@ -48,7 +48,7 @@ int Window::Initialise()
 		glfwTerminate();
 		return 1;
 	}
-	//Obtener tamaño de Buffer
+	//Obtener tamaÃ±o de Buffer
 	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
 
 	//asignar el contexto
@@ -63,7 +63,7 @@ int Window::Initialise()
 
 	if (glewInit() != GLEW_OK)
 	{
-		printf("Falló inicialización de GLEW");
+		printf("FallÃ³ inicializaciÃ³n de GLEW");
 		glfwDestroyWindow(mainWindow);
 		glfwTerminate();
 		return 1;
@@ -74,7 +74,7 @@ int Window::Initialise()
 							 
 							 //Asignar Viewport
 	glViewport(0, 0, bufferWidth, bufferHeight);
-	//Callback para detectar que se está usando la ventana
+	//Callback para detectar que se estÃ¡ usando la ventana
 	glfwSetWindowUserPointer(mainWindow, this);
 }
 
@@ -108,7 +108,7 @@ void Window::ManejaTeclado(GLFWwindow* window, int key, int code, int action, in
 	}
 	if (key == GLFW_KEY_P && action == GLFW_RELEASE)  //encender luces quiosko
 	{
-		theWindow->luces_quiosko_prendidas= !theWindow->luces_quiosko_prendidas;
+		theWindow->luces_quiosko_prendidas= !(theWindow->luces_quiosko_prendidas);
 	}
 	if (key == GLFW_KEY_M && action == GLFW_RELEASE)  //cambiar camara
 	{
